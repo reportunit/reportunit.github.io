00090b4716700fbd26de5b2c3ff239421534c891
anshooarora <anshooarora@gmail.com>
2015-05-29 08:55:25 -0500

    Completely revamped the structure to be minimal, added usage examples and most of the links

--- scripts/script.js
+++
@@ -1,5 +0,0 @@
-$(function() {
-	$('#footer').load('./modules/footer.html');
-	$('.menu').load('./modules/menu.html');
-	$('h2').append('<div class="head-border-small"></div><div class="head-border-large"></div>');
-});
